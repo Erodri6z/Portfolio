4136aac14fac62a4a9820ed25d8fa2aaa2ea58d8
Erodri6z <erikrodriguez2417@gmail.com>
2022-06-09 11:10:50 -0500

    fix resume bug

--- js/app.js
+++ js/app.js
@@ -4,7 +4,21 @@ const skill = document.getElementById('skill')
 const project = document.getElementById('project')
 const contact = document.getElementById('contact')
 const modeBtn = document.getElementById('light-dark')
-const name = document.getElementById('name')
+const about = document.getElementById('name')
 const title = document.getElementById('title')
 const resume = document.getElementById('id')
 const contactLink = document.getElementById('contacts')
+const list = document.getElementById('skill-list')
+
+
+// info.addEventListener('click',function(){
+//     modeBtn.scrollIntoView(true)
+//  })
+//  skill.addEventListener('click',function(){
+//      list.scrollIntoView(true)
+// })
+//info.addEventListener('click',function(){
+//     resume.scrollIntoView(true)
+// })info.addEventListener('click',function(){
+//     resume.scrollIntoView(true)
+// })
