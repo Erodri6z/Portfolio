d6c5dc34397338111607d7aa2b60c5927acd49fe
Erodri6z <erikrodriguez2417@gmail.com>
2022-06-09 19:49:23 -0500

    add tic tac toe

--- js/app.js
+++ js/app.js
@@ -12,22 +12,21 @@ const list = document.getElementById('skill-list')
 const body = document.querySelector('body')
 
 
-
 info.addEventListener('click',function(){
-     modeBtn.scrollIntoView({behavior:"smooth"})
+    modeBtn.scrollIntoView({behavior:"smooth"})
 })
 skill.addEventListener('click',function(){
       about.scrollIntoView({behavior:"smooth"})
- })
-project.addEventListener('click',function(){
-     resume.scrollIntoView({behavior:"smooth"})
- })
-contact.addEventListener('click',function(){
-     contactLink.scrollIntoView({behavior:"smooth"})
-})
-modeBtn.addEventListener('click',toggleLightDark)
-
-
+    })
+    project.addEventListener('click',function(){
+        resume.scrollIntoView({behavior:"smooth"})
+    })
+    contact.addEventListener('click',function(){
+        contactLink.scrollIntoView({behavior:"smooth"})
+    })
+    modeBtn.addEventListener('click',toggleLightDark)
+    
+    
 function checkDarkPref(){
     if(window.matchMedia("(prefers-color-scheme:dark)").matches &&
     body.className !== 'dark'
@@ -38,4 +37,23 @@ function checkDarkPref(){
 function toggleLightDark(){
     body.className = body.className === "dark" ? "" : "dark"
 }
+
+import { projectData } from "./project-data.js"
+
+const myStuff = document.getElementById('my-stuff')
+
+let projectMarkup = projectData.map(projectData =>
+    ` 
+        <div>
+            <a href="${projectData.deployment}">
+                <img id="ttt" src="${projectData.image}" alt="${projectData.title}">
+            </a>
+            <div>
+                <p>${projectData.description}</p>
+            </div>
+        </div>
+`
+).join('')
+
+myStuff.innerHTML = projectMarkup
 
---
+++ js/project-data.js
@@ -0,0 +1,14 @@
+let projectData = [{
+
+    title: 'Tic-Tac-Toe',
+    deployment: 'https://ttt-erodri6z.netlify.app/',
+    image: '/assets/tictactoe.png',
+    github:'https://github.com/Erodri6z/TicTacToe',
+    description: 'An application to play tic-tac-toe.'
+}
+]
+
+
+export{
+    projectData
+}
