4ffeb42b472438043fb20168e2d3711e99f742e4
Erodri6z <erikrodriguez2417@gmail.com>
2022-08-24 22:13:44 -0500

    fix projects images

--- js/project-data.js
+++ js/project-data.js
@@ -2,7 +2,7 @@ let projectData = [{
 
     title: 'Tic-Tac-Toe',
     deployment: 'https://ttt-erodri6z.netlify.app/',
-    image: '/assets/tictactoe.png',
+    image: '/assets/ttt.png',
     github:'https://github.com/Erodri6z/TicTacToe',
     description: 'An application to play tic-tac-toe.'
 },{
