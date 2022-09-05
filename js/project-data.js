d6c57eeb6dd0d2643c6fa3b5d0fd0ea1c09ca4b2
Erodri6z <erikrodriguez2417@gmail.com>
2022-09-05 11:41:32 -0500

    add better project descriptions

--- js/project-data.js
+++ js/project-data.js
@@ -4,25 +4,26 @@ let projectData = [{
     deployment: 'https://ttt-erodri6z.netlify.app/',
     image: '/assets/ttt.png',
     github:'https://github.com/Erodri6z/TicTacToe',
-    description: 'An application to play tic-tac-toe.'
+    description: 'Fully coded in javascript, CSS, and HTML. Built a minimal Tic-Tac-Toe browser game. Learned the basics of app logic as well as app deployment'
+    
 },{
     title: "Skill-Tree",
     deployment: 'https://erodri6z-skill-tree.herokuapp.com/',
     image: '/assets/SkillTree.png',
     github: 'https://github.com/Erodri6z/Skill-Tree',
-    description: 'An Application to collect your skills and comment on others.'
+    description: 'This was an app meant to gather your skills and share them with others. Used full MEN stack, HTML, CSS, JavaScript, GoogleAuth, and EJS. This app helped me learn about GoogleAuth, ERDs, and Heroku deployment'
 }, {
     title: "Campfire",
     deployment: 'https://campfire-erodri6z.herokuapp.com/',
     image: '/assets/homepage.png',
     github: 'https://github.com/Erodri6z/CampFire',
-    description: 'An Application to be able to share and get ideas for creative writting.'
+    description: 'An app that can be used to share and find story ideas. Used Django, Python3, Docker, HTML and CSS. This app helped me learn Python and PostgreSQL'
 }, {
     title: "Stranger Drinks",
     deployment: 'https://stranger-drinks.netlify.app/',
     image: '/assets/strangerdrinks.png',
     github: 'https://github.com/psison92/stranger-drinks-front-end/issues',
-    description: 'A React app where you can share drink ideas, this was a group project of whom I was a contributer to.'
+    description: 'It’s a “Stranger Things”  themed app to be able to share drink ideas and recipies. An MERN stack app built in collaboration with 3 other teammates. Used full MERN stack, HTML, CSS, and Javascript. This app helped me learn React.js, JWT authentication, and Github group workflow'
 }
 ]
 
